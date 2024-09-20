bd353b15d538e53ea1787369665bc69df1521cc4
sergiobarrio <81761973+BarrioXx@users.noreply.github.com>
2024-09-20 19:34:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -20,35 +20,6 @@ document.addEventListener("DOMContentLoaded", function() {
             // Mostramos la sección correspondiente al enlace clicado
             const sectionToShow = this.getAttribute('data-section');
             document.getElementById(sectionToShow).classList.add('visible');
-
-            new Swiper('.card-wrapper', {
-                loop: true,
-                spaceBetween: 10,
-                
-                // If we need pagination
-                pagination: {
-                    el: '.swiper-pagination',
-                },
-                
-                // Navigation arrows
-                navigation: {
-                    nextEl: '.swiper-button-next',
-                    prevEl: '.swiper-button-prev',
-                },
-            
-                breakpoints: {
-                    0: {
-                        slidesPerView: 1
-                    },
-                    768: {
-                        slidesPerView: 2
-                    },
-                    1024: {
-                        slidesPerView: 3
-                    },
-                }
-            
-            });
         });
     });
 });
